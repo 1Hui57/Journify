89fda4b6106aad15628ae5917d02f163d228ddee
1Hui57 <melody870507@gmail.com>
2025-05-26 04:18:58 +0800

    style: Update the style of phone

--- src/component/TripAttractionWrapper.tsx
+++ src/component/TripAttractionWrapper.tsx
@@ -19,7 +19,7 @@ export default function TripAttractionWrappwer({ tripDaySchedule, timestampToDat
 
     return (
         <div className="w-full h-fit px-4 ">
-            <div className="w-fit h-fit py-2 ml-3 mt-3 mb-3 text-base-700 text-myzinc-900 border- rounded-md">{`第${tripDaySchedule.number}天`}</div>
+            <div className="w-fit h-fit py-2 ml-3 mt-1 mb-1 md:mt-3 md:mb-3 text-base-700 text-myzinc-900 border- rounded-md">{`第${tripDaySchedule.number}天`}</div>
             {tripDaySchedule.attractionData && tripDaySchedule.attractionData.length > 0 &&
                 tripDaySchedule.attractionData.map((tripScheduleItem, index) => {
                     return (
