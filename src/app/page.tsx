0564fe047aa725d8e5592353229deb53a087200a
1Hui57 <melody870507@gmail.com>
2025-07-28 12:40:33 +0800

    fix: Fix the HomTripCard key error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
     const [hotCountries, setHotCountries] = useState<HotCounty[] | null>(null);
 
     const { addUserId, userDataMap } = useUserData();
-    const founderId = process.env.NEXT_PUBLIC_FOUNDER_ID as string;
+    const founderId:string = process.env.NEXT_PUBLIC_FOUNDER_ID ?? '';
 
     const defaultCoverPhotos = [
         "/default1.jpg",
